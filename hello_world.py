ae30270f63d34115ef14f26a38b84efa6505c2a0
Chloe Akerblom <akerblomch@wgc.school.nz>
2019-02-15 10:03:18 +1300

    Added more labels and configured colours and fonts.

--- hello_world.py
+++ hello_world.py
@@ -2,10 +2,23 @@ import tkinter
 root = tkinter.Tk()
 
 root.title("Hello World!")
-root.geometry("600x200")
+root.geometry("600x600")
 
 my_label = tkinter.Label(root)
-my_label.config(text="Hello!", fg="teal")
+my_label.config(text="Hello!", fg="teal", bg="gold", font=("Stencil","40"), padx="5", bd="40")
 my_label.grid()
+
+my_label = tkinter.Label(root)
+my_label.config(text="Hello!", fg="pink", bg="black", font=("Stencil","40"), padx="5", bd="40")
+my_label.grid()
+
+my_label = tkinter.Label(root)
+my_label.config(text="Hello!", fg="purple", bg="orange", font=("Stencil","40"), padx="5", bd="40")
+my_label.grid()
+
+my_label = tkinter.Label(root)
+my_label.config(text="Hello!", fg="white", bg="green", font=("Stencil","40"), padx="5", bd="40")
+my_label.grid()
+
 
 root.mainloop()
